18ceeb263ebd2c5dd38f1f7124505ee861f31be6
Alex K <777ak888@gmail.com>
2025-09-16 05:59:40 +0000

    modified:   calculator.py
            added division function
            added template for main function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
+
 def division(a, b):
     '''
     Function returns the quotient of a divided by b.
     '''
+    return(a/b)
 
 def multiplication(a, b):
     '''
@@ -9,10 +11,13 @@ def multiplication(a, b):
     '''
     return(a * b)
 
+def main():
+    print('I can calculate one of four operations "+ - * /" on two numbers.')
+    input_expr = input("Please, enter your expression as 'a <operator> b':")
+    expr_lst = input_expr.split(' ')
+    print(expr_lst)
 
 if __name__ == "__main__":
 
-    result = 0
-
-    print(result)
+    main()
 
