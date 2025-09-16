e0b2c95b1bd56dd90afad528e1abbc0fa1b7a2bc
Alex K <777ak888@gmail.com>
2025-09-16 09:03:48 +0000

    modified:   calculator.py         all functions are collected together         input expression checks are added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+import re
+
+def is_expression_valid(expression):
+    pattern = r'^\d+(?:\.\d+)?\s[+\-*/]\s\d+(?:\.\d+)?$'
+    return bool(re.match(pattern, expression))
 
 def division(a, b):
     '''
@@ -18,10 +23,26 @@ def addition(a, b):
     return(a + b)
 
 def main():
+    operators = ["+", "-", "*", "/"]
+
     print('I can calculate one of four operations "+ - * /" on two numbers.')
-    input_expr = input("Please, enter your expression as 'a <operator> b':")
-    expr_lst = input_expr.split(' ')
-    print(expr_lst)
+    input_expr = input("Please, enter your expression as 'a <operator> b': ")
+
+    if is_expression_valid(input_expr):
+        expr_lst = input_expr.split(' ')
+
+        if expr_lst[1] == '+': print(f"Result of {input_expr} = {addition(float(expr_lst[0]), float(expr_lst[2]))}")
+        elif expr_lst[1] == '-': print(f"Result of {input_expr} = {addition(float(expr_lst[0]), float(expr_lst[2]))}")
+        elif expr_lst[1] == '*': print(f"Result of {input_expr} = {multiplication(float(expr_lst[0]), float(expr_lst[2]))}")
+        elif expr_lst[1] == '/': print(f"Result of {input_expr} = {division(float(expr_lst[0]), float(expr_lst[2]))}")
+        else:
+            print(f'I can calculate one of four operations "+ - * /" on two numbers, not {expr_lst[1]}.')
+    else:
+        print("Your expression is not passed validity check!")
+        print("Please, enter your expression as 'a <operator> b' <- spaces are impotant!")
+        print(f"But your expression is {input_expr}.")
+
+    print(input_expr.split(' '))
 
 if __name__ == "__main__":
 
